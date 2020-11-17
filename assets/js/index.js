204e0e9a92ef7601df999e67b17f2eefabf63727
Laman R. Quliyeva <QuliyevaLR@kapitalho.az>
2020-11-17 17:17:52 +0400

    implement add, remove functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,41 +1,88 @@
+let todos = [
+  // {
+  //    id: 0,
+  //    text: 'todo1',
+  //    isCompleted: false
+  // },
+  // {
+  //    id: 1,
+  //    text: "todo2",
+  //    isCompleted: true
+  // }
+];
 
-const todos = [
-   // {
-   //    id: 0,
-   //    text: 'todo1',
-   //    isCompleted: false
-   // },
-   // {
-   //    id: 1,
-   //    text: "todo2",
-   //    isCompleted: true
-   // }
-]
+const addItemBtn = document.querySelector(".add-item");
+const newItemInp = document.querySelector(".new-item");
+const list = document.querySelector(".list");
 
-const addItemBtn = document.querySelector('.add-item');
-const newItemInp = document.querySelector('.new-item');
-const list = document.querySelector('.list');
+const generateId = () =>
+  Math.floor(Math.random() * Math.floor(Math.random() * Date.now()));
 
-const generateId = ()=> Math.floor(Math.random() * Math.floor(Math.random() * Date.now()));
-
-function addTodo(e) {
-   e.preventDefault();
-   const id = generateId();
-   const text = newItemInp.value;
-   const newTodo = {
-      id,
-      text,
-      isCompleted: false
-   }
-   todos.push(newTodo);
-
+function addTodo(id) {
+  const text = newItemInp.value;
+  const newTodo = {
+    id,
+    text,
+    isCompleted: false,
+  };
+  todos.push(newTodo);
 }
 
-function addToList(){
-let listItem = document.createElement("LI");
-   listItem.textContent = newItem;
-   list.appendChild(listItem)
+function addToList(id) {
+  const listItem = document.createElement("LI");
+  const removeBtn = document.createElement("button");
+
+  listItem.textContent = newItemInp.value;
+  listItem.setAttribute("id", `${id}`);
+  removeBtn.textContent = "X";
+
+  removeBtn.addEventListener("click", onRemoveItem);
+  listItem.appendChild(removeBtn);
+  list.appendChild(listItem);
 }
 
+// function showList() {
+//   list.querySelectorAll("*").forEach((item) => item.remove());
+//   todos.map((todo) => {
+//     const listItem = document.createElement("LI");
+//     const removeBtn = document.createElement("button");
 
-addItemBtn.addEventListener("click", addTodo);
+//     listItem.textContent = newItemInp.value;
+//     listItem.setAttribute("id", `${todo.id}`);
+//     removeBtn.textContent = "X";
+
+//     removeBtn.addEventListener("click", onRemoveItem);
+//     listItem.appendChild(removeBtn);
+//     list.appendChild(listItem);
+//   });
+// }
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  const id = generateId();
+  if (!newItemInp.value) return;
+  addTodo(id);
+  addToList(id);
+  //   showList();
+  newItemInp.value = "";
+  console.log(todos);
+}
+
+function removeTodo(id) {
+  console.log(id);
+  todos = todos.filter((todo, index) => todo.id != id);
+  //console.log(updatedTodo);
+}
+
+function removeFromList(item) {
+  console.log(item);
+  item.remove();
+}
+
+function onRemoveItem(e) {
+  const id = e.target.parentNode.id;
+  removeTodo(id);
+  removeFromList(e.target.parentNode);
+}
+
+addItemBtn.addEventListener("click", onSubmitForm);
